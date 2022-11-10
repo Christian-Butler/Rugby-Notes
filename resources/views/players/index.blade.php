155383dd93a13437f32315c4f1a29e8b2edb014e
Christian Butler <N00211989@iadt.ie>
2022-11-10 15:23:16 +0000

    added and unadded daisy ui link

--- resources/views/players/index.blade.php
+++ resources/views/players/index.blade.php
@@ -1,13 +1,21 @@
+
+ 
 <x-app-layout>
+    <head>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+ </head>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Players') }}
         </h2>
     </x-slot>
 
+    
+
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto col-sm-6 sm:px-6 lg:px-8">
             {{-- <x-alert-success>
                 {{ session('success') }}
             </x-alert-success> --}}
