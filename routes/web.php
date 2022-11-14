7486342abcbc97109a9629968dca29d691242b50
Christian Butler <N00211989@iadt.ie>
2022-11-14 19:09:21 +0000

    changed controller class to admin and user controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\PlayerController;
+use App\Http\Controllers\PlayerController as AdminPlayerController;
+use App\Http\Controllers\PlayerController as UserPlayerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,3 +26,8 @@ Route::get('/dashboard', function () {
 Route::resource('/players',PlayerController::class)->middleware(['auth']);
 
 require __DIR__.'/auth.php';
+
+Route::resource('/admin/players', AdminPlayerController::class)->middleware(['auth'])->names('admin.players');
+
+Route::resource('/user/players', UserBookController::class)->middleware(['auth'])->names('user.players')->only(['index', 'show']);
+
